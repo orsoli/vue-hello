430dd57d71a0bbefe236880bf925fcb272890bff
orsoli <fljcoli@gmail.com>
2024-09-03 15:05:46 +0200

    Print message in page in H1 element

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
+/*
+  Display a message on the screen inside an h1 element, using data.
+
+  BONUS: Add an image to the page, also sourced from the data.
+*/
+
 // Create a Veu app
 const { createApp } = Vue;
 
 createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      message: "First exercise using Veu.js Framework", // Create Message
     };
   },
 }).mount("#app");
